90728e4e8f385c7d1e007189a3cb5e9a8e8fe10e
Orr Avrech <orravrech@gmail.com>
2024-06-05 15:18:18 -0400

    start textual inversion in instuct-pix2pix with diffusers implementation

--- magnification/datasets.py
+++ magnification/datasets.py
@@ -1,6 +1,8 @@
+import torch
 from pathlib import Path
 from PIL import Image
 from torch.utils.data import Dataset
+import numpy as np
 
 
 class AFHQ(Dataset):
@@ -10,10 +12,10 @@ class AFHQ(Dataset):
         self.transform = transform
         self.cat_dir = self.split_dir / "cat"
         self.dog_dir = self.split_dir / "dog"
-        
+
         self.cat_images = list(self.cat_dir.rglob("*.jpg"))
         self.dog_images = list(self.cat_dir.rglob("*.jpg"))
-        
+
         self.image_list = self.cat_images + self.dog_images
         self.labels = [0] * len(self.cat_images) + [1] * len(self.dog_images)
 
@@ -22,14 +24,14 @@ class AFHQ(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.image_list[idx]
-        image = Image.open(img_path).convert('RGB')
+        image = Image.open(img_path).convert("RGB")
         label = self.labels[idx]
-        
+
         if self.transform:
             image = self.transform(image)
-        
+
         return image, label
-    
+
 
 class Cats(AFHQ):
     def __init__(self, root_dir: Path, split: str, transform=None):
@@ -43,3 +45,68 @@ class Dogs(AFHQ):
         super().__init__(root_dir, split, transform)
         self.image_list = self.dog_images
         self.labels = [1] * len(self.dog_images)
+
+
+class ImageEdits(Dataset):
+    def __init__(self, images_dir: Path, edits_dir: Path, transform=None) -> None:
+        self.images_dir = images_dir
+        self.edits_dir = edits_dir
+        self.transform = transform
+
+        self.image_list = list(self.images_dir.rglob("*"))
+
+    def __len__(self):
+        return len(self.image_list)
+
+    @staticmethod
+    def convert_to_np(img_path: Path):
+        image = Image.open(img_path).convert("RGB")
+        return np.array(image).transpose(2, 0, 1)
+
+    def __getitem__(self, idx):
+        img_path = self.image_list[idx]
+        img_edit_path = self.edits_dir / img_path.name
+
+        image = self.convert_to_np(img_path)
+        image_edit = self.convert_to_np(img_edit_path)
+
+        images = np.concatenate([image, image_edit])
+        images = torch.tensor(images)
+        images = 2 * (images / 255) - 1
+
+        if self.transform:
+            images = self.transform(images)
+
+        image, image_edit = images.chunk(2)
+        return image, image_edit
+
+
+class TextualInversionEdits(ImageEdits):
+    def __init__(
+        self,
+        images_dir: Path,
+        edits_dir: Path,
+        placeholder_str: list[str],
+        transform=None,
+    ) -> None:
+        super().__init__(images_dir, edits_dir, transform)
+        self.placeholder_str = placeholder_str
+
+    def __getitem__(self, idx):
+        img_path = self.image_list[idx]
+        img_edit_path = self.edits_dir / img_path.name
+
+        image = self.convert_to_np(img_path)
+        image_edit = self.convert_to_np(img_edit_path)
+
+        images = np.concatenate([image, image_edit])
+        images = torch.tensor(images)
+        images = 2 * (images / 255) - 1
+
+        if self.transform:
+            images = self.transform(images)
+
+        image, image_edit = images.chunk(2)
+        # TODO: support multiple placeholder strings
+        prompt = self.placeholder_str[0]
+        return image, image_edit, prompt
---
+++ magnification/textual_inversion_config.py
@@ -0,0 +1,36 @@
+from typing import Optional
+from pathlib import Path
+from dataclasses import dataclass
+
+
+@dataclass
+class EmbeddingManagerConfig:
+    placeholder_strings: list
+    initializer_words: list
+    per_image_tokens: bool
+    num_vectors_per_token: int
+    progressive_words: bool
+
+
+@dataclass
+class DatasetConfig:
+    image_dir: Path
+    image_edits_dir: Path
+    repeats: int
+    img_size: Optional[int] = 256
+
+
+@dataclass
+class LDMConfig:
+    embedding_config: EmbeddingManagerConfig
+    conditioning_dropout_prob: Optional[float] = None
+
+
+@dataclass
+class TextualInversionConfig:
+    diffusion: LDMConfig
+    dataset: DatasetConfig
+    batch_size: int
+    epochs: int
+    learning_rate: float
+    device: int
